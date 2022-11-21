df56a44c55bab1cb3ac521a8b7e0f7ae1014a557
9t5c <uucc@live.com>
2022-11-21 16:35:54 +0800

    🐛 HoverReverso

--- playground/react/src/App.tsx
+++ playground/react/src/App.tsx
@@ -1,9 +1,15 @@
-import { useRipple } from '@butter/react'
+import { useHoverReverso, useRipple } from '@butter/react'
 
 function App() {
-  const ref = useRipple()
+  const ref = useHoverReverso()
 
-  return <button ref={ref}>this is button</button>
+  return (
+    <button>
+      <div style={{ width: '100%', height: '100%' }} ref={ref}>
+        this is button
+      </div>
+    </button>
+  )
 }
 
 export default App
